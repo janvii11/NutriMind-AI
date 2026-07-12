8ebae4f18033fe632780758cf144bf5e224f4f0a
janvi agarwal <ajanvi114@gmail.com>
2026-07-12 12:51:27 +0530

    Fixed Firebase cloud config

--- firebase_config.py
+++ firebase_config.py
@@ -1,13 +1,16 @@
+import streamlit as st
 import firebase_admin
+
 from firebase_admin import credentials, firestore
-from pathlib import Path
 
-BASE_DIR = Path(__file__).resolve().parent
-
-KEY_PATH = BASE_DIR / "firebase_key.json"
 
 if not firebase_admin._apps:
-    cred = credentials.Certificate(KEY_PATH)
+
+    firebase_key = st.secrets["FIREBASE_KEY"]
+
+    cred = credentials.Certificate(dict(firebase_key))
+
     firebase_admin.initialize_app(cred)
+
 
 db = firestore.client()
