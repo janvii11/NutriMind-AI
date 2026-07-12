d5eae712b49e63076c35e7f16b66401edb5046eb
janvi agarwal <ajanvi114@gmail.com>
2026-07-12 19:18:09 +0530

    Debug firebase config

--- firebase_config.py
+++ firebase_config.py
@@ -1,16 +1,26 @@
 import streamlit as st
 import firebase_admin
-
 from firebase_admin import credentials, firestore
-
+from pathlib import Path
 
 if not firebase_admin._apps:
 
-    firebase_key = st.secrets["FIREBASE_KEY"]
+    try:
+        
 
-    cred = credentials.Certificate(dict(firebase_key))
+        st.write(type(st.secrets["FIREBASE_KEY"]))
+        st.write(st.secrets["FIREBASE_KEY"].keys())
+        st.stop()
+        # Streamlit Cloud
+        cred = credentials.Certificate(dict(st.secrets["FIREBASE_KEY"]))
+
+    except Exception:
+        # Local Laptop
+        BASE_DIR = Path(__file__).resolve().parent
+        KEY_PATH = BASE_DIR / "firebase_key.json"
+
+        cred = credentials.Certificate(KEY_PATH)
 
     firebase_admin.initialize_app(cred)
-
 
 db = firestore.client()
